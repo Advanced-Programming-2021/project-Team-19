d4d057ba93dada39752abc2a5f161b09931723c6
Taha1506 <tahaakbarialvanagh@gmail.com>
2021-04-16 22:10:57 +0430

    My commit

---
+++ project-Team-19/project/src/main/java/Controller/ScoreBoardMenuController.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import Model.User;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+
+class ScoreBoardMenuController {
+    class UserComp implements Comparator<User>{
+        public int compare(User firstUser,User secondUser){
+            return 1;
+        }
+    }
+    private TreeSet<User> allUsers=new TreeSet<>(new UserComp());
+    private  void showScores(Matcher showScoresMatcher){
+
+    }
+    public void addUser(User user){
+
+    }
+    public void removeUser(User user){
+
+    }
+    public void mangeScoreBoardMenu(){
+
+    }
+}
