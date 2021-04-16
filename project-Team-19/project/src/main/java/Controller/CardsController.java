df6b6791302d508297ca071091062e5bda6f3574
Taha1506 <tahaakbarialvanagh@gmail.com>
2021-04-16 23:49:14 +0430

    Pushed

---
+++ project-Team-19/project/src/main/java/Controller/CardsController.java
@@ -0,0 +1,62 @@
+package Controller;
+
+import java.util.regex.Matcher;
+
+class CardsController {
+    private Card card;
+    private User user;
+    private static CardsController instance=null;
+    public static CardsController getInstance(){
+        if(instance==null){
+            instance=new CardsController();
+        }
+        return instance;
+    }
+    private void summonMonster(Matcher matcher){
+
+
+    }
+    private void setMonster(Matcher matcher){
+
+    }
+    private void changeMode(Matcher matcher){
+
+
+    }
+    private void flipSummon(Matcher matcher){
+
+    }
+    private void attackMonster(Matcher matcher){
+
+    }
+    private void attackDirect(Matcher matcher){
+
+    }
+    private void activateEffect(Matcher matcher){
+
+    }
+    private void setSpell(Matcher matcher){
+
+    }
+    private void setTrap(Matcher matcher){
+
+    }
+    private void activateSpellOrTrap(Matcher matcher){
+
+    }
+    private void ritualSummon(Matcher matcher){
+
+    }
+    private void specialSummon(Matcher matcher){
+
+    }
+    private void showGraveYard(Matcher matcher){
+
+    }
+    private void showAllCards(Matcher matcher){
+
+    }
+    private void handleScores(Matcher matcher){
+
+    }
+}
