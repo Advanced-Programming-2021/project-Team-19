90281c6e0cf6e8ee4bfe4a75f02ffeb834be0eb9
Taha1506 <tahaakbarialvanagh@gmail.com>
2021-05-01 14:44:51 +0430

    out.print -->Printer.print

--- project-Team-19/project/src/main/java/Controller/DuelController.java
+++ project-Team-19/project/src/main/java/Controller/DuelController.java
@@ -5,6 +5,7 @@ import Controller.DataBaseControllers.UserDataBaseController;
 import Model.Gamer;
 import Model.User;
 import View.GetInput;
+import View.Printer.Printer;
 
 import java.util.regex.Matcher;
 
@@ -28,22 +29,22 @@ class DuelController {
             User rival= UserDataBaseController.getUserByUsername(matcher.group(1));
             int turn=Integer.parseInt(matcher.group(2));
             if(rival==null){
-                System.out.println("there is no player with this username");
+                Printer.print("there is no player with this username");
             }
             else if(user.getActiveDeckName()==null){
-                System.out.println(user.getUsername()+"has no active deck");
+                Printer.print(user.getUsername()+"has no active deck");
             }
             else if(rival.getActiveDeckName()==null){
-                System.out.println(rival.getUsername()+"has no active deck");
+                Printer.print(rival.getUsername()+"has no active deck");
             }
             else if(!DeckDataBaseController.getDeckByName(user.getUsername()+"_"+user.getActiveDeckName()).isDeckValid()){
-                System.out.println(user.getUsername()+"’s deck is invalid");
+                Printer.print(user.getUsername()+"’s deck is invalid");
             }
             else if(!DeckDataBaseController.getDeckByName(rival.getUsername()+"_"+rival.getActiveDeckName()).isDeckValid()){
-                System.out.println(rival.getUsername()+"’s deck is invalid");
+                Printer.print(rival.getUsername()+"’s deck is invalid");
             }
             else if(turn!=1&&turn!=3){
-                System.out.println("number of rounds is not supported");
+                Printer.print("number of rounds is not supported");
             }
             else{
                 Gamer firstGamer=new Gamer(user);
@@ -58,13 +59,13 @@ class DuelController {
             commandIsDone=true;
             int turn=Integer.parseInt(matcher.group(1));
             if(user.getActiveDeckName()==null){
-                System.out.println(user.getUsername()+"has no active deck");
+                Printer.print(user.getUsername()+"has no active deck");
             }
             else if(!DeckDataBaseController.getDeckByName(user.getUsername()+"_"+user.getActiveDeckName()).isDeckValid()){
-                System.out.println(user.getUsername()+"’s deck is invalid");
+                Printer.print(user.getUsername()+"’s deck is invalid");
             }
             else if(turn!=1&&turn!=3){
-                System.out.println("number of rounds is not supported");
+                Printer.print("number of rounds is not supported");
             }
             else{
 
