1507233f61b556dff7f54167c361ef64d3c6c8d1
Taha1506 <tahaakbarialvanagh@gmail.com>
2021-05-04 14:31:46 +0430

    push

--- project-Team-19/project/src/main/java/Model/Gamer.java
+++ project-Team-19/project/src/main/java/Model/Gamer.java
@@ -4,31 +4,34 @@ import Controller.DataBaseControllers.DeckDataBaseController;
 
 public class Gamer {
     AllBoards gameBoard = new AllBoards();
-    private int lifePoint=4000;
+    private int lifePoint = 4000;
     private User user;
     private Deck deck;
 
-    public Gamer(User user){
+    public Gamer(User user) {
         this.user = user;
         //this.deck= (Deck) DeckDataBaseController.getDeckByName(user.getActiveDeckName()).clone();
     }
 
-    public int getLifePoint(){
+    public int getLifePoint() {
         return lifePoint;
     }
 
-    public String selfToString(){
-        return gameBoard.selfToString()+
-                user.getNickname()+":"+lifePoint;
+    public String selfToString() {
+        return gameBoard.selfToString() +
+                user.getNickname() + ":" + lifePoint;
     }
-    public String rivalToString(){
-        return user.getNickname()+":"+lifePoint+"\n"+
+
+    public String rivalToString() {
+        return user.getNickname() + ":" + lifePoint + "\n" +
                 gameBoard.rivalToString();
     }
-    public void increaseLifePoint(int amount){
-        this.lifePoint+=amount;
+
+    public void increaseLifePoint(int amount) {
+        this.lifePoint += amount;
     }
-    public void decreaseLifePoint(int amount){
-        this.lifePoint+=amount;
+
+    public void decreaseLifePoint(int amount) {
+        this.lifePoint += amount;
     }
 }
