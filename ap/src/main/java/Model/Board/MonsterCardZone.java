6811ccbe3edf0ff0f80c7e67f210b971f925c6b3
Taha1506 <tahaakbarialvanagh@gmail.com>
2021-05-07 15:41:43 +0430

    hoho

--- ap/src/main/java/Model/Board/MonsterCardZone.java
+++ ap/src/main/java/Model/Board/MonsterCardZone.java
@@ -1,23 +1,22 @@
 package Model.Board;
 
 import Model.Card.Card;
+import Model.Card.Monster;
 import Model.Enums.CardMod;
 
 import java.util.ArrayList;
 
 public class MonsterCardZone extends Zones {
-    private ArrayList<String> cardsInMonsterZone = new ArrayList<>();
-    private ArrayList<CardMod> cardsMods = new ArrayList<>();
+    private final ArrayList<Monster> cardsInMonsterZone = new ArrayList<>();
 
     {
         for (int i = 0; i < 5; i++) {
             cardsInMonsterZone.add(null);
-            cardsMods.add(CardMod.EMPTY);
         }
     }
 
 
-    public String getCardById(int id, boolean selfRequest) {
+    public Card getCardById(int id, boolean selfRequest) {
         if (selfRequest) {
             return cardsInMonsterZone.get(hashNumber(id));
         } else {
@@ -47,13 +46,13 @@ public class MonsterCardZone extends Zones {
     public String toString(){
         StringBuilder temp= new StringBuilder("\t");
         for(int i=0;i<5;i++){
-            if(cardsMods.get(i).equals(CardMod.EMPTY)){
+            if(cardsInMonsterZone.get(i).getCardMod().equals(CardMod.EMPTY)){
                 temp.append("E\t");
             }
-            else if(cardsMods.get(i).equals(CardMod.DEFENSIVE_HIDDEN)){
+            else if(cardsInMonsterZone.get(i).getCardMod().equals(CardMod.DEFENSIVE_HIDDEN)){
                 temp.append("DH\t");
             }
-            else if(cardsMods.get(i).equals(CardMod.DEFENSIVE_OCCUPIED)){
+            else if(cardsInMonsterZone.get(i).getCardMod().equals(CardMod.DEFENSIVE_OCCUPIED)){
                 temp.append("DO\t");
             }
             else{
