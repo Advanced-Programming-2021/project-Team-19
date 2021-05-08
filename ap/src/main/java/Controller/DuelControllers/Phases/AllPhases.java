c034c1443fdf08c731215ba036b686bc1ffa9452
Taha1506 <tahaakbarialvanagh@gmail.com>
2021-05-08 15:25:25 +0430

    hoho

--- ap/src/main/java/Controller/DuelControllers/Phases/AllPhases.java
+++ ap/src/main/java/Controller/DuelControllers/Phases/AllPhases.java
@@ -12,7 +12,7 @@ public class AllPhases {
     String phaseName;
     protected GameData gameData;
 
-    public void select(String command) {
+    public void select(String command,int turn) {
         commandIsDone = false;
         selectMonster(Utils.getMatcher(command, "select --monster (\\d)"));
         selectOpponentMonster(Utils.getMatcher(command, "select (?=.*?--monster)(?=.*?--opponent)--\\S+ --\\S+ (\\d)"));
